5f34ca728aee0894840066eb5aba9dcee1897052
Yuriy Gonzales Molina <vebster88@mail.ru>
2020-07-26 00:50:20 +0300

    Добавлен метод выявляющий при помощи рекурсии максимальное число в массиве

--- src/Sum.java
+++ src/Sum.java
@@ -1,8 +1,9 @@
 public class Sum {
     public static void main(String[] args) {
-        Integer[] arr = {1, 3, 7, 10, 12, 1, 4, 34 , 56};
+        Integer[] arr = {1, 3, 7, 10, 1212, 1, 4, 34 , 56};
         System.out.println("Сумма чисел массива: " + sumArr(arr));
         System.out.println("Количество элементов в списке: " + sumOfNum(arr));
+        System.out.println("Наибольшее число в списке: " + maxNumber(arr));
 
 
     }
@@ -44,9 +45,25 @@ public class Sum {
             }
             sum = sum + sumOfNum(arr1);
         }
-
-
     return sum;
+    }
+
+    public static int maxNumber(Integer[] arr) {
+        int max = arr[0];
+        if (arr.length == 0||arr.length==1) {
+            return max;
+        } else {
+            Integer[] arr1 = new Integer[arr.length-1];
+            for (int i=1; i<arr.length; i++) {
+                arr1[i-1] = arr[i];
+            }
+            int max1 = maxNumber(arr1);
+            if (max < max1) {
+                max = max1;
+            }
+        }
+
+    return max;
     }
 
 }
