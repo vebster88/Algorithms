5bc03a257ff0af1c099c279635a8aa2e7c223655
Yuriy Gonzales Molina <vebster88@mail.ru>
2020-07-26 00:35:41 +0300

    Добавил в Класс Sum новый метод, который рекурсивно считает количество элементов массива.

--- src/Sum.java
+++ src/Sum.java
@@ -1,7 +1,8 @@
 public class Sum {
     public static void main(String[] args) {
-        Integer[] arr = {1, 3, 7, 10, 12};
-        System.out.println(sumArr(arr));
+        Integer[] arr = {1, 3, 7, 10, 12, 1, 4, 34 , 56};
+        System.out.println("Сумма чисел массива: " + sumArr(arr));
+        System.out.println("Количество элементов в списке: " + sumOfNum(arr));
 
 
     }
@@ -23,13 +24,29 @@ public class Sum {
             return sum;
         } else {
             Integer[] arr1 = new Integer[arr.length-1];
-            for (int i=1;i<arr.length;i++) {
+            for (int i=1; i<arr.length; i++) {
                 arr1[i-1] = arr[i];
             }
             sum = sum + sumArr(arr1);
         }
         return sum;
 
+    }
+
+    public static int sumOfNum(Integer[] arr) {
+        int sum = 1;
+        if (arr.length == 0||arr.length ==1) {
+            return sum;
+        } else {
+            Integer[] arr1 = new Integer[arr.length-1];
+            for (int i=1; i<arr.length; i++) {
+                arr1[i-1] = arr[i];
+            }
+            sum = sum + sumOfNum(arr1);
+        }
+
+
+    return sum;
     }
 
 }
