ad51682df5dfe282939c2b8d0b5d508069bcb579
Yuriy Gonzales Molina <vebster88@mail.ru>
2020-07-28 18:59:23 +0300

    added search_queue method

--- src/WideSearch.java
+++ src/WideSearch.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class WideSearch {
+
+    public static void main(String[] args) {
+        HashMap<String, String[]> widesearch = new HashMap<>();
+        Set<String> names = new HashSet<>(Arrays.asList("me", "bob", "alice", "bob", "claire", "anuj", "peggy", "thom", "jonny"));
+
+
+        String[] you = {"alice", "bob", "claire"};
+        String[] bob = {"anuj", "peggy"};
+        String[] alice = {"peggy"};
+        String[] claire = {"thom", "jonny"};
+        String[] anuj = {};
+        String[] peggy = {};
+        String[] thom = {};
+        String[] jonny = {};
+
+        widesearch.put("me", you);
+        widesearch.put("alice", alice);
+        widesearch.put("bob", bob);
+        widesearch.put("claire", claire);
+        widesearch.put("anuj", anuj);
+        widesearch.put("peggy", peggy);
+        widesearch.put("thom", thom);
+        widesearch.put("jonny", jonny);
+
+        search_queue("me", widesearch);
+
+    }
+    public static LinkedList<String> search_queue(String human, Map<String, String[]> map) {
+        LinkedList<String> deque = new LinkedList<>();
+        String[] arr = map.get(human);
+        for (String p: arr) {
+            deque.addLast(p);
+        }
+        while (!deque.isEmpty()) {
+
+
+        }
+
+        return new LinkedList<>();
+
+    }
+
 }
